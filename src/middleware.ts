2ae259dd35e908840291050c66da9b03c6ce67f4
junhee <dlfmadmfeo@gmail.com>
2025-05-06 16:54:58 +0900

    middleware 추가

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("access-token")?.value;
+  const pathname = req.nextUrl.pathname;
+  const urls = ["/kakao/login", "/kakao/signup"];
+
+  if (!token && !urls.includes(pathname)) {
+    return NextResponse.redirect(new URL("/kakao/login", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+// middleware가 적용될 경로 설정
+export const config = {
+  matcher: ["/((?!_next|favicon.ico|api|static).*)"],
+};
