9bd28b75e401fb8ebccb89aae858393c4911cadf
junhee <dlfmadmfeo@gmail.com>
2025-08-02 20:23:38 +0900

    notfound 기능 추가

---
+++ src/app/[id]/user/not-found.tsx
@@ -0,0 +1,14 @@
+export default function NotFound() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh"
+      }}
+    >
+      404 에러입니다. (By 조준희)
+    </div>
+  );
+}
