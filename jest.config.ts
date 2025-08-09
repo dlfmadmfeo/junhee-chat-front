2d33216fb8bfd7fb3b87fc7ef926999b2016ca57
junhee <dlfmadmfeo@gmail.com>
2025-08-09 10:28:34 +0900

    jest config 추가

--- jest.config.ts
+++ jest.config.ts
@@ -10,10 +10,11 @@ const createJestConfig = nextJest({
 const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleNameMapper: {
-    '^@/components/(.*)$': '<rootDir>/components/$1',
-  }
+    "^@/components/(.*)$": "<rootDir>/components/$1",
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
