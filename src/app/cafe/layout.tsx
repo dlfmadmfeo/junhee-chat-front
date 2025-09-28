4fdaf9ae630f9fa15fdc407640a78a10a2ab237e
junhee <dlfmadmfeo@gmail.com>
2025-09-28 19:27:56 +0900

    메타데이터 추가(title, description, ...)

--- src/app/cafe/layout.tsx
+++ src/app/cafe/layout.tsx
@@ -1,6 +1,14 @@
 'use client';
 import { HomeIcon } from '@heroicons/react/24/outline';
+import { Metadata } from 'next';
 import { useRouter, usePathname } from 'next/navigation';
+
+export const metadata: Metadata = {
+  title: '준희의 카페',
+  description: '카페 메뉴를 선택하여 장바구니에 담고 결제할 수 있는 앱입니다.',
+  keywords: ['junhee cafe'],
+  authors: [{ name: '조준희' }, { name: 'junhee92kr' }],
+};
 
 export default function CafeLayout({
   children,
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import './globals.css';
 import Head from 'next/head';
 import Toast from '@/components/Toast';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '채팅앱/카페앱',
+  description: '채팅앱과 카페앱을 개발하는 사이트입니다.',
+  keywords: ['junhee chat', 'junhee cafe'],
+  authors: [{ name: '조준희' }, { name: 'junhee92kr' }],
+};
 
 export default function RootLayout({
   children,
