d4a2f9899d2cd623f303baf42437e41fa2261ff0
junhee <dlfmadmfeo@gmail.com>
2025-09-28 22:54:03 +0900

    google search seo를 위한 sitemap 추가

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://junhee92kr.com', // 본인 도메인
+  generateRobotsTxt: true, // robots.txt 자동 생성
+  sitemapSize: 7000,
+};
